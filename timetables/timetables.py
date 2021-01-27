9ff705ba15e1fe211ce9387643ce2766ec53053a
Lorenzo Morelli <lorenzo.morelli.bs@gmail.com>
2021-01-27 01:02:13 +0100

    implemented minimum timespan, fixed #2

--- timetables/timetables.py
+++ timetables/timetables.py
@@ -1,22 +1,18 @@
 ### TIMETABLE MATCHING
 
 # A: [[9:00, 11:30], [12:30, 15:00], [16:00, 18:00]]
-# B: [[9:30, 11:00], [15:30, 16:30], [17:00, 18:30]]
+# B: [[9:30, 11:00], [15:20, 16:30], [17:00, 18:30]]
+# Minimum: 30
 
-# output: [11.30, 12.30], [15.00, 15.30]
+# output: [[11:30, 12:30]]
 
 def compare(time1, time2):
     hour1, minute1 = time1.split(":")
     hour2, minute2 = time2.split(":")
     time1 = int(hour1) * 60 + int(minute1)
     time2 = int(hour2) * 60 + int(minute2)
-
-    if time1 > time2:
-        return 1
-    elif time1 == time2:
-        return 0
-    elif time1 < time2:
-        return -1
+    diff = time2 - time1
+    return diff
 
 
 def unavailable(sched1, sched2):
@@ -24,7 +20,7 @@ def unavailable(sched1, sched2):
     p1 = 0
     p2 = 0
     while p1 < len(sched1) or p2 < len(sched2) - 1:
-        if compare(sched1[p1][0], sched2[p2][0]) == -1:
+        if compare(sched1[p1][0], sched2[p2][0]) > 0:
             booked.append(sched1[p1])
             p1 += 1
         else:
@@ -36,9 +32,9 @@ def unavailable(sched1, sched2):
 def remove_repetitions(booked):
     i = 0
     while i < len(booked) - 1:
-        if compare(booked[i][1], booked[i + 1][1]) == 1:
+        if compare(booked[i][1], booked[i + 1][1]) <= 0:
             booked.pop(i+1)
-        elif compare(booked[i][1], booked[i + 1][0]) == 1:
+        elif compare(booked[i][1], booked[i + 1][0]) <= 0:
             booked[i][1] = booked[i + 1][1]
             booked.pop(i+1)
         else:
@@ -46,22 +42,24 @@ def remove_repetitions(booked):
     return booked
 
 
-def available(booked):
+def available(booked, min):
     avail = []
     i = 0
     while i < len(booked) - 1:
-        freespace = []
-        freespace.append(booked[i][1])
-        freespace.append(booked[i + 1][0])
-        avail.append(freespace)
+        if compare(booked[i][1], booked[i+1][0]) >= min:
+            freespace = []
+            freespace.append(booked[i][1])
+            freespace.append(booked[i + 1][0])
+            avail.append(freespace)
         i += 1
     return avail
 
 
 # TEST
 schedule1 = [["9:00", "11:30"], ["12:30", "15:00"], ["16:00", "18:00"]]
-schedule2 = [["9:30", "11:00"], ["15:30", "16:30"], ["17:00", "18:30"]]
+schedule2 = [["9:30", "11:00"], ["15:20", "16:30"], ["17:00", "18:30"]]
+minimum = 30
 full = unavailable(schedule1, schedule2)
 full = remove_repetitions(full)
-free = available(full)
+free = available(full, minimum)
 print(free)
